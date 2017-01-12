e3c654621187358bfc2671f163e78aa9c6eff924
Jonathan Guthrie <jono@perfect.org>
2017-01-12 17:08:44 -0500

    Update to v1, add CSRF tests

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2016-12-15 19:55:52 +0000
+// Date: 2017-01-12 22:05:38 +0000
 import PackageDescription
 let package = Package(
     name: "Perfect-Session-PostgreSQL-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 0, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 1, minor: 0),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
     ]
 )
