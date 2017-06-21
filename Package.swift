3ecb63ada261f2781f17034637a2bd3bb2f00d77
Jonathan Guthrie <jono@perfect.org>
2017-06-21 14:47:43 -0400

    update minor versions

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-01-12 22:05:38 +0000
+// Date: 2017-03-09 21:06:01 +0000
 import PackageDescription
 let package = Package(
     name: "Perfect-Session-PostgreSQL-Demo",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 1, minor: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Session-PostgreSQL.git", majorVersion: 1),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-Repeater.git", majorVersion: 1),
     ]
 )
